dabc2ac1f9a89ad5303c5cbf000f871333fa25fd
João Vitor Caversan <joaopassos@alunos.utfpr.edu.br>
2024-07-02 18:20:19 -0300

    Changes arrays names in sprayField component.

--- fpgaGraphicSprayingSimulator/sprayField.vhd
+++ fpgaGraphicSprayingSimulator/sprayField.vhd
@@ -25,8 +25,8 @@ end sprayField;
 architecture sprayField_arch of sprayField is
    type square is array(0 to (squaresQty - 1)) of std_logic_vector(3 downto 0);
    type ticksArray is array(0 to (squaresQty - 1)) of unsigned(31 downto 0);
-   signal squares : square := (others => "0000");
-   signal ticks : ticksArray := (others => (others => '0'));
+   signal squareMoist : square := (others => "0000");
+   signal ticksInSquare : ticksArray := (others => (others => '0'));
    signal ticksForRGBDarkeningU : unsigned(31 downto 0);
    signal tractorXInt : integer;
    signal tractorYInt : integer;
@@ -48,66 +48,66 @@ begin
    process(clk)
    begin
       if rising_edge(clk) then
-         if ticks(squareIndexForTractor) = ticksForRGBDarkeningU then
+         if ticksInSquare(squareIndexForTractor) = ticksForRGBDarkeningU then
             if tractorXInt < screenWidth and tractorYInt < screenHeight then
-               if squares(squareIndexForTractor) < "0101" then
-                  squares(squareIndexForTractor) <= std_logic_vector(unsigned(squares(squareIndexForTractor)) + 1);
+               if squareMoist(squareIndexForTractor) < "0101" then
+                  squareMoist(squareIndexForTractor) <= std_logic_vector(unsigned(squareMoist(squareIndexForTractor)) + 1);
                end if;
             end if;
-            ticks(squareIndexForTractor) <= (others => '0');
+            ticksInSquare(squareIndexForTractor) <= (others => '0');
          else
-            ticks(squareIndexForTractor) <= ticks(squareIndexForTractor) + 1;
+            ticksInSquare(squareIndexForTractor) <= ticksInSquare(squareIndexForTractor) + 1;
          end if;
       end if;
    end process;
 
    squareIndexForPixel <= ((to_integer(unsigned(pixelX)) / squareWidth) + ((to_integer(unsigned(pixelY)) / squareWidth) * squareQtyInOneRow));
-   red <= "1011" when squares(squareIndexForPixel) = "0000" else
-       "1001" when squares(squareIndexForPixel) = "0001" else
-       "0111" when squares(squareIndexForPixel) = "0010" else
-       "0110" when squares(squareIndexForPixel) = "0011" else
-       "0100" when squares(squareIndexForPixel) = "0100" else
-       "0110" when squares(squareIndexForPixel) = "0101" else
-       "0100" when squares(squareIndexForPixel) = "0110" else
-       "0100" when squares(squareIndexForPixel) = "0111" else
-       "0111" when squares(squareIndexForPixel) = "1000" else
-       "1000" when squares(squareIndexForPixel) = "1001" else
-       "1010" when squares(squareIndexForPixel) = "1010" else
-       "0110" when squares(squareIndexForPixel) = "1011" else
-       "1001" when squares(squareIndexForPixel) = "1100" else
-       "0110" when squares(squareIndexForPixel) = "1101" else
+   red <= "1011" when squareMoist(squareIndexForPixel) = "0000" else
+       "1001" when squareMoist(squareIndexForPixel) = "0001" else
+       "0111" when squareMoist(squareIndexForPixel) = "0010" else
+       "0110" when squareMoist(squareIndexForPixel) = "0011" else
+       "0100" when squareMoist(squareIndexForPixel) = "0100" else
+       "0110" when squareMoist(squareIndexForPixel) = "0101" else
+       "0100" when squareMoist(squareIndexForPixel) = "0110" else
+       "0100" when squareMoist(squareIndexForPixel) = "0111" else
+       "0111" when squareMoist(squareIndexForPixel) = "1000" else
+       "1000" when squareMoist(squareIndexForPixel) = "1001" else
+       "1010" when squareMoist(squareIndexForPixel) = "1010" else
+       "0110" when squareMoist(squareIndexForPixel) = "1011" else
+       "1001" when squareMoist(squareIndexForPixel) = "1100" else
+       "0110" when squareMoist(squareIndexForPixel) = "1101" else
        "1110";
 
-green <= "0110" when squares(squareIndexForPixel) = "0000" else
-         "0101" when squares(squareIndexForPixel) = "0001" else
-         "0100" when squares(squareIndexForPixel) = "0010" else
-         "0011" when squares(squareIndexForPixel) = "0011" else
-         "0011" when squares(squareIndexForPixel) = "0100" else
-         "0011" when squares(squareIndexForPixel) = "0101" else
-         "0100" when squares(squareIndexForPixel) = "0110" else
-         "0110" when squares(squareIndexForPixel) = "0111" else
-         "0100" when squares(squareIndexForPixel) = "1000" else
-         "0100" when squares(squareIndexForPixel) = "1001" else
-         "0100" when squares(squareIndexForPixel) = "1010" else
-         "0100" when squares(squareIndexForPixel) = "1011" else
-         "0011" when squares(squareIndexForPixel) = "1100" else
-         "0011" when squares(squareIndexForPixel) = "1101" else
+green <= "0110" when squareMoist(squareIndexForPixel) = "0000" else
+         "0101" when squareMoist(squareIndexForPixel) = "0001" else
+         "0100" when squareMoist(squareIndexForPixel) = "0010" else
+         "0011" when squareMoist(squareIndexForPixel) = "0011" else
+         "0011" when squareMoist(squareIndexForPixel) = "0100" else
+         "0011" when squareMoist(squareIndexForPixel) = "0101" else
+         "0100" when squareMoist(squareIndexForPixel) = "0110" else
+         "0110" when squareMoist(squareIndexForPixel) = "0111" else
+         "0100" when squareMoist(squareIndexForPixel) = "1000" else
+         "0100" when squareMoist(squareIndexForPixel) = "1001" else
+         "0100" when squareMoist(squareIndexForPixel) = "1010" else
+         "0100" when squareMoist(squareIndexForPixel) = "1011" else
+         "0011" when squareMoist(squareIndexForPixel) = "1100" else
+         "0011" when squareMoist(squareIndexForPixel) = "1101" else
          "0011";
 
-blue <= "0110" when squares(squareIndexForPixel) = "0000" else
-        "0101" when squares(squareIndexForPixel) = "0001" else
-        "0100" when squares(squareIndexForPixel) = "0010" else
-        "0011" when squares(squareIndexForPixel) = "0011" else
-        "0011" when squares(squareIndexForPixel) = "0100" else
-        "0011" when squares(squareIndexForPixel) = "0101" else
-        "0100" when squares(squareIndexForPixel) = "0110" else
-        "0110" when squares(squareIndexForPixel) = "0111" else
-        "0100" when squares(squareIndexForPixel) = "1000" else
-        "0100" when squares(squareIndexForPixel) = "1001" else
-        "0100" when squares(squareIndexForPixel) = "1010" else
-        "0100" when squares(squareIndexForPixel) = "1011" else
-        "0011" when squares(squareIndexForPixel) = "1100" else
-        "0011" when squares(squareIndexForPixel) = "1101" else
+blue <= "0110" when squareMoist(squareIndexForPixel) = "0000" else
+        "0101" when squareMoist(squareIndexForPixel) = "0001" else
+        "0100" when squareMoist(squareIndexForPixel) = "0010" else
+        "0011" when squareMoist(squareIndexForPixel) = "0011" else
+        "0011" when squareMoist(squareIndexForPixel) = "0100" else
+        "0011" when squareMoist(squareIndexForPixel) = "0101" else
+        "0100" when squareMoist(squareIndexForPixel) = "0110" else
+        "0110" when squareMoist(squareIndexForPixel) = "0111" else
+        "0100" when squareMoist(squareIndexForPixel) = "1000" else
+        "0100" when squareMoist(squareIndexForPixel) = "1001" else
+        "0100" when squareMoist(squareIndexForPixel) = "1010" else
+        "0100" when squareMoist(squareIndexForPixel) = "1011" else
+        "0011" when squareMoist(squareIndexForPixel) = "1100" else
+        "0011" when squareMoist(squareIndexForPixel) = "1101" else
         "0011";
 
 
